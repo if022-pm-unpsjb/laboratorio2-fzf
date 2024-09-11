e96081962f1243ad20204d2c7d5efd83224ef74e
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-09-11 20:00:13 -0300

    update de compras

--- lib/compras.ex
+++ lib/compras.ex
@@ -1,9 +1,42 @@
+# En este modulo, cada vez que se usa id, es el id de COMPRA
+# los demas se especifican como id_producto
 defmodule Libremarket.Compras do
 
   def comprar() do
     {:show_me_the_money}
   end
 
+  def iniciar_compra() do
+    {:show_me_the_money}
+  end
+
+  def seleccionar_producto() do
+    {:select_items}
+  end
+
+  def seleccionar_entrega() do
+    x = :rand.uniform(100)
+    if (x>=20) do
+      {:ok,"correo"}
+    else
+      {:ok,"retiro"}
+    end
+  end
+  
+  def confirmar_compra() do
+    x = :rand.uniform(100)
+    if (x>=30) do
+      # confirma la compra
+      {:ok}
+    else
+      # no confirma la compra
+      {:error}
+    end
+  end
+ 
+  def seleccionar_pago() do
+    {:metodo_de_pago}
+  end 
 end
 
 defmodule Libremarket.Compras.Server do
@@ -26,7 +59,20 @@ defmodule Libremarket.Compras.Server do
     GenServer.call(pid, :comprar)
   end
 
+  def seleccionar_producto(pid \\ __MODULE__) do
+    GenServer.call(pid, :seleccionar_producto)
+  end
+
+  def seleccionar_entrega(pid \\ __MODULE__) do
+    GenServer.call(pid, :seleccionar_entrega)
+  end
+
+  def seleccionar_pago(pid \\ __MODULE__) do
+    GenServer.call(pid, :seleccionar_pago)
+  end
+
   # Callbacks
+  # EL STATE DE COMPRAS ES LA LISTA DE COMPRAS, como diccionario xddd
 
   @doc """
   Inicializa el estado del servidor
@@ -45,4 +91,46 @@ defmodule Libremarket.Compras.Server do
     {:reply, result, state}
   end
 
+  def handle_call({:seleccionar_producto,id,id_producto}, _from, state) do
+    Libremarket.Compras.seleccionar_pago()
+    Libremarket.Compras.Server.confirmar_compra()
+    #estara bien devolver esto?
+    {:reply, state, state}
+  end
+
+  def handle_call(:seleccionar_entrega, _from, state) do
+    metodo_entrega = Libremarket.Compras.seleccionar_entrega()
+    case metodo_entrega do
+      {:ok, "correo"} -> Libremarket.Envios.Server.calcular_costo() 
+      # falta llamar a seleccionar pago desde aca no?
+      # tambien deberia guardar en el esto que responde 
+      {:ok, "retiro"} -> Libremarket.Compras.Server.seleccionar_pago()
+    {:reply, state, state}
+  end
+
+  def handle_call(:seleccionar_pago, _from, state) do
+    Libremarket.Compras.seleccionar_pago()
+    Libremarket.Compras.Server.confirmar_compra()
+    #estara bien devolver esto?
+    {:reply, state, state}
+  end
+
+  def handle_call(:confirmar_compra, _from, state) do
+    result = Libremarket.Compras.confirmar_compra()
+    # aca deberia buscar en el estado, con el id de compra, al resultado del la consulta de infracciones
+    {:reply, result, state}
+  end
+
+
+
+    """
+    notix:
+      esto empieza con iniciar compra, me devuelve un id, que yo se lo pase tambien ese id,
+    ahi inicia un diccionario con ese id,
+    Despues le hago el seleccionar_producto, le paso el id del producto y el id de la compra
+
+    seria como stateless
+
+    """
 end
+
