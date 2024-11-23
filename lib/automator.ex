5f579dbf0f72ef1ec8f00c3501ace4a77118da4a
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-11-22 21:19:47 -0300

    rangos de automator

--- lib/automator.ex
+++ lib/automator.ex
@@ -12,7 +12,7 @@ defmodule Automator do
       1..cantidad_compras
       |> Enum.map(fn _ ->
         id_compra = :rand.uniform(1000)
-        id_producto = :rand.uniform(15)
+        id_producto = :rand.uniform(11)
         metodoDeEntrega = seleccionar_entrega()
         metodoDePago = seleccionar_pago()
         realizar_compra(id_compra, id_producto, metodoDeEntrega, metodoDePago)
@@ -25,7 +25,7 @@ defmodule Automator do
       for _ <- 1..cantidad_compras do
         spawn (fn ->
         id_compra = :rand.uniform(1000)
-        id_producto = :rand.uniform(15)
+        id_producto = :rand.uniform(11)
         metodoDeEntrega = seleccionar_entrega()
         metodoDePago = seleccionar_pago()
 
